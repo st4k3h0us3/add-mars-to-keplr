ef4942622a01a7d4a9ab6c89793451dfef557489
larry <26318510+larry0x@users.noreply.github.com>
2022-07-05 10:17:25 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,7 @@ declare global {
   interface Window extends KeplrWindow {}
 }
 
-const addBtn = document.getElementById("addBtn") as HTMLButtonElement;
-const addNonStandardBtn = document.getElementById("addNonStandardBtn") as HTMLButtonElement;
+const btn = document.getElementById("btn") as HTMLButtonElement;
 
 const marsToken = {
   coinDenom: "MARS",
@@ -16,44 +15,33 @@ const marsToken = {
   coinGeckoId: "mars-protocol",
 };
 
-const keplrChainInfo = (chainName: string, coinType = 118) => ({
-  chainId: "mars-1",
-  chainName: chainName,
-  rpc: "https://rpc.marsprotocol.io",
-  rest: "https://rest.marsprotocol.io",
-  bip44: {
-    coinType,
-  },
-  bech32Config: {
-    bech32PrefixAccAddr: "mars",
-    bech32PrefixAccPub: "mars" + "pub",
-    bech32PrefixValAddr: "mars" + "valoper",
-    bech32PrefixValPub: "mars" + "valoperpub",
-    bech32PrefixConsAddr: "mars" + "valcons",
-    bech32PrefixConsPub: "mars" + "valconspub",
-  },
-  currencies: [marsToken],
-  feeCurrencies: [marsToken],
-  stakeCurrency: marsToken,
-  coinType,
-  gasPriceStep: {
-    low: 0,
-    average: 0,
-    high: 0,
-  },
-});
-
-window.onload = async () => {
-  if (!window.keplr) {
-    alert("Please install keplr extension");
+btn.addEventListener("click", () => {
+  if (window.keplr) {
+    window.keplr.experimentalSuggestChain({
+      chainId: "mars-1",
+      chainName: "Mars",
+      rpc: "https://rpc.marsprotocol.io",
+      rest: "https://rest.marsprotocol.io",
+      bip44: {
+        coinType: 118,
+      },
+      bech32Config: {
+        bech32PrefixAccAddr: "mars",
+        bech32PrefixAccPub: "mars" + "pub",
+        bech32PrefixValAddr: "mars" + "valoper",
+        bech32PrefixValPub: "mars" + "valoperpub",
+        bech32PrefixConsAddr: "mars" + "valcons",
+        bech32PrefixConsPub: "mars" + "valconspub",
+      },
+      currencies: [marsToken],
+      feeCurrencies: [marsToken],
+      stakeCurrency: marsToken,
+      coinType: 118,
+      gasPriceStep: {
+        low: 0,
+        average: 0,
+        high: 0,
+      },
+    });
   }
-
-  addBtn.addEventListener("click", () => {
-    window.keplr.experimentalSuggestChain(keplrChainInfo("Mars", 118));
-  });
-
-  addNonStandardBtn.addEventListener("click", () => {
-    console.log('adding nonstandard')
-    window.keplr.experimentalSuggestChain(keplrChainInfo("Mars (non-standard)", 330));
-  });
-};
+});
